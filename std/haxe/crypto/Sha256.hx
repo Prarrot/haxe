15d2cf575c24286769fad38c1cb4d0e45ae2475b
Tim <tim@ent.net>
2017-04-18 21:53:40 -0700

    4x speed Optimization Sha224.hx, Sha256.hx
    Inline and @:extern on heavily used micro-functions in SHA 224 and 256.

--- std/haxe/crypto/Sha256.hx
+++ std/haxe/crypto/Sha256.hx
@@ -148,39 +148,47 @@ class Sha256 {
 		blks[nblk * 16 - 1] = b.length * 8;
 		return blks;
 	}
-
+	@:extern
 	inline function S(X, n) {
 		return ( X >>> n ) | (X << (32 - n));
 	}
 
+	@:extern
 	inline function R(X, n) {
 		return ( X >>> n );
 	}
 
+	@:extern
 	inline function Ch(x, y, z) {
 		return ((x & y) ^ ((~x) & z));
 	}
 
+	@:extern
 	inline function Maj(x, y, z) {
 		return ((x & y) ^ (x & z) ^ (y & z));
 	}
 
+	@:extern
 	inline function Sigma0256(x) {
 		return (S(x, 2) ^ S(x, 13) ^ S(x, 22));
 	}
 
+	@:extern
 	inline function Sigma1256(x) {
 		return (S(x, 6) ^ S(x, 11) ^ S(x, 25));
 	}
 
+	@:extern
 	inline function Gamma0256(x) {
 		return (S(x, 7) ^ S(x, 18) ^ R(x, 3));
 	}
 
+	@:extern
 	inline function Gamma1256(x) {
 		return (S(x, 17) ^ S(x, 19) ^ R(x, 10));
 	}
 
+	@:extern
 	inline function safeAdd(x, y) {
 		var lsw = (x & 0xFFFF) + (y & 0xFFFF);
 		var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
